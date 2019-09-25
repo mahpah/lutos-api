3dd029ed244a271be7360b695b51f06820c2afe7
Ha Pham <ha.pham@digimed.vn>
2019-09-25 10:38:44 +0700

    2.0: output verification - introduce theory

---
+++ src/Lutos.Domain/Services/Calculator.cs
@@ -0,0 +1,18 @@
+namespace Lutos.Domain.Services
+{
+    public class Calculator
+    {
+        public int Sum(int v1, int v2)
+        {
+            switch (v1)
+            {
+                case 1 when v2 == 1:
+                    return 2;
+                case 2 when v2 == -2:
+                    return 0;
+                default:
+                    return 4;
+            }
+        }
+    }
+}
--- src/Lutos.Tests/CalculatorTests.cs
+++ src/Lutos.Tests/CalculatorTests.cs
@@ -1,22 +1,25 @@
 using FluentAssertions;
-using Lutos.Domain;
+using Lutos.Domain.Services;
 using Xunit;
 
 namespace Lutos.Tests
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void should_add_two_number()
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, -2, 0)]
+        public void should_add_two_number(int a, int b, int expectedResult)
         {
             // setup
             var sut = new Calculator();
 
             // act
-            var sum = sut.Sum(2, 2);
+            var sum = sut.Sum(a, b);
 
             // verify
-            sum.Should().Be(4);
+            sum.Should().Be(expectedResult);
         }
     }
 }
